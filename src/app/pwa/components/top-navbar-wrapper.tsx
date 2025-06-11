29dff076924411fa16c91c2fb78338011cfbf7c2
elmoghost <ghostsln@proton.me>
2025-06-11 16:29:19 -0400

    remove pwa dir

--- src/app/pwa/components/top-navbar-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-export function TopNavbarWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-background px-safe h-t-nav mt-t-inset fixed top-0 right-0 left-0 z-50 flex w-full justify-center border-b">
-      {children}
-    </div>
-  );
-}
--- src/app/pwa/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ThemeProvider } from "@/providers/theme-provider";
-
-export function PwaProviders({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
-}
